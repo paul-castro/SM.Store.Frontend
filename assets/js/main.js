2ea3658c182d9f9034a0472ff8711c5106980142
Paul Castro <paul.castro@gaitcon.com>
2020-02-25 10:36:51 +0800

    update burger menu

---
+++ assets/js/main.js
@@ -0,0 +1,68 @@
+$(document).ready(function(){
+    // WIDGET ANIMATION
+    $(window).scroll(() => {
+        // console.log($(this).scrollTop());
+        var fromTop = $(this).scrollTop();
+        var widgetHeight = $('.widgets').height();
+        if(fromTop >= widgetHeight * 2) {
+            $('.widgets').css('margin-bottom', '-' + fromTop + 'px');
+        }
+
+        else {
+            $('.widgets').css('margin-bottom', fromTop + 'px');
+        }
+    });
+
+
+    // BURGER MENU
+    const $menu = $('.burger-menu, .nav__mobile, .nav__mobile-sub, .nav__mobile *, .nav__mobile-sub *');
+    $(document).mouseup(function (e) {
+        var win = $(window);
+        if (!$menu.is(e.target)) {
+            $('.burger-menu-default').prop('hidden', false);
+            $('.burger-menu-close').prop('hidden', true);
+            $('.nav__mobile').css('display', 'none');
+            $('.nav__mobile-sub').css('display', 'none');
+            $('.nav__mobile').css('left', '12px');
+        }
+    });
+    $('.burger-menu').click(function(){
+        if($('.nav__mobile').css('display') == 'none'){
+            $('.nav__mobile').css('display', 'flex');
+            $('.burger-menu-default').prop('hidden', true);
+            $('.burger-menu-close').prop('hidden', false);
+        }
+        else {
+            $('.nav__mobile').css('display', 'none');
+            $('.nav__mobile-sub').css('display', 'none');
+            $('.burger-menu-default').prop('hidden', false);
+            $('.burger-menu-close').prop('hidden', true);
+            $('.nav__mobile').css('left', '12px');
+        }
+    });
+
+    // CHANGE BURGER MENU CONTENT
+    $('.nav__mobile-upper-item').click(function(){
+        var submenu = $(this).data('item');
+        $('.nav__mobile').css('left', '-360px');
+        $('.nav__mobile--'+submenu).css('display', 'flex');
+    });
+    $('.nav__mobile-control-back').click(function(){
+        $('.nav__mobile').css('left', '12px');
+        $('.nav__mobile-sub').css('display', 'none');
+    });
+
+
+    // SLIM SCROLL FOR CUSTOM SCROLLBAR
+    $('.nav__mobile-content').slimScroll({
+        height: '345px',
+        width: '288px',
+        alwaysVisible: true,
+        railVisible: true,
+        railColor: '#E0E0E0',
+        railOpacity: 1,
+        color: '#0067B3',
+        opacity: 1
+    });
+
+});
